c7c00b75d5b0025cfcf1b9604fa123c994da8341
LAXMIKANT SARASWAT <laxmikantsarswat007@gmail.com>
2021-08-04 17:26:20 +0530

    Initial commit

---
+++ src/main/java/com/example/iunitconverter/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.iunitconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    private TextView textView4;
+    private EditText editText;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView4 = findViewById(R.id.textView4);
+        editText = findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Created By Laxmikant ", Toast.LENGTH_SHORT).show();
+                String s = editText.getText().toString();
+                int kg = Integer.parseInt(s);
+                double pound = 2.20462 * kg;
+                textView4.setText("The corresponding Pounds value is: "+ pound);
+
+
+            }
+        });
+
+
+
+
+    }
+}
